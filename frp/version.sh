ae12dc4a5958548e19f99401667ef692aa8af28c
hitsword <mail@huayizhiyun.com>
2018-09-02 00:01:45 +0800

    first

---
+++ frp/version.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export FRP_VER=0.21.0
+export huayi_download_url="https://soft.huayizhiyun.com/manage/frp"
+export github_download_url="https://github.com/fatedier/frp/releases/download"
